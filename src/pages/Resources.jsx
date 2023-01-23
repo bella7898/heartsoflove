1735f33de8923cfa97c181698b158059f5c76301
L-Mariam <l.mariam.here@gmail.com>
2023-01-23 20:42:55 +0400

    Resources/Shop

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,23 +1,61 @@
 import React from 'react';
 import "../index.css";
+import "./resources.css";
 
 function Resources() {
     return (
+        <>
+        <div class="bg"></div>
         <div class="content">
-            <section class="rimg">
-                <h1>Resources</h1>
-            </section>
-            <section class="rintro">
-                <h1>Newsletters</h1>
-                <img src="placeholder.jpg"></img>
-                <h1>Blogs</h1>
-                <img src="placeholder.jpg"></img>
-                <h1>Chapters</h1>
-                <img src="placeholder.jpg"></img>
-                <h1>Podcasts</h1>
-                <img src="placeholder.jpg"></img>
-            </section>
+        <section class="cards">
+            {/* card 1 */}
+            <a href="" title="Go to our Newsletters">
+                <article class="card">
+                    <h1> Newsletters </h1>
+                    <div class="card-content">
+                    <img src="placeholder.jpg" alt="" /> 
+                    </div>
+                </article>
+            </a>
+            {/* card 2 */}
+            <a href="" title="Go to our Newsletters">
+                <article class="card">
+                    <h1> Blogs </h1>
+                    <div class="card-content">
+                    <img src="placeholder.jpg" alt="" /> 
+                    </div>
+                </article>
+            </a>
+            {/* card 3 */}
+            <a href="" title="Go to our Newsletters">
+                <article class="card">
+                    <h1> Chapters </h1>
+                    <div class="card-content">
+                    <img src="placeholder.jpg" alt="" /> 
+                    </div>
+                </article>
+            </a>
+            {/* card 4 */}
+            <a href="" title="Go to our Newsletters">
+                <article class="card">
+                    <h1> <br></br> </h1>
+                    <div class="card-content">
+                    <img src="placeholder.jpg" alt="" /> 
+                    </div>
+                </article>
+            </a>
+            {/* card 5 */}
+            <a href="" title="Go to our Newsletters">
+                <article class="card">
+                    <h1> Podcasts </h1>
+                    <div class="card-content">
+                    <img src="placeholder.jpg" alt="" /> 
+                    </div>
+                </article>
+            </a>
+        </section>
         </div>
+        </>
     );
 }
 
