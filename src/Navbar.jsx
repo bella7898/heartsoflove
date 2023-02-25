21c478f3eeaccd8b488319a12933c26b98e175b3
L-Mariam <l.mariam.here@gmail.com>
2023-02-25 22:28:20 +0400

    Contact Us Popup Form
    - Popup code is in the Navbar.jsx file along with the 'contact us' button.
     - Added two new files under 'Components' folder in 'pages' containing the js and css file for the popup.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,13 @@ import "./index.css";
 
 import logo from "./assets/Hearts_2.png"
 
+import Popup from './pages/components/Popup';
+import {useState} from 'react';
+
+
 function Navbar() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+
   return (
     <header>
       <div class="left">
@@ -17,8 +23,19 @@ function Navbar() {
           <Link to="/join">Join Us</Link>
           <Link to="/resources">Resources</Link>
           <Link to="/shop">Shop</Link>
-          <Link>Contact Us</Link>
+          <button onClick={() => setButtonPopup(true)}> Contact Us </button>  
       </div>
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+      <div className='popup-content'>
+      <img src={logo}></img>
+        <h1> Contact Us!</h1>
+        <p> Need to contact us? 
+        <br></br><br></br>
+        Email: heartsofloveorganisation@gmail.com <br></br>
+        Instagram: @hearts_of_love_organization</p>
+        </div>
+      </Popup>
     </header>
   );
 }
